134b7424ece913b66c65fae50d02c11a08741c64
chandandigital6 <chandandigital6@gmail.com>
2026-08-26 13:51:20 +0530

    test

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
@@ -9,6 +10,20 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 
+
+Schedule::command(
+    'followups:send-reminder-notifications'
+)
+    ->everyMinute()
+    ->withoutOverlapping();
+
+Schedule::call(function () {
+    Log::info('HOSTINGER AUTOMATIC CRON WORKING', [
+        'server_time' => now()->toDateTimeString(),
+    ]);
+})
+    ->name('hostinger-automatic-cron-test')
+    ->everyMinute();
 
 Schedule::command(
     'followups:send-reminder-notifications'
