f9e7598ee9162ebaf6c3641e19a865a2aea4da93
tabrej03dz <tabrejalam770496@gmail.com>
2026-09-05 12:39:34 +0530

    remember me

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,8 +1,10 @@
 <x-layouts::auth :title="__('Log in')">
     <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+        <x-auth-header
+            :title="__('Log in to your account')"
+            :description="__('Enter your email and password below to log in')"
+        />
 
-        <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
 
         <x-passkey-verify />
@@ -10,7 +12,6 @@
         <form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-6">
             @csrf
 
-            <!-- Email Address -->
             <flux:input
                 name="email"
                 :label="__('Email address')"
@@ -22,7 +23,6 @@
                 placeholder="email@example.com"
             />
 
-            <!-- Password -->
             <div class="relative">
                 <flux:input
                     name="password"
@@ -35,17 +35,26 @@
                 />
 
                 @if (Route::has('password.request'))
-                    <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>
+                    <flux:link
+                        class="absolute top-0 text-sm end-0"
+                        :href="route('password.request')"
+                        wire:navigate
+                    >
                         {{ __('Forgot your password?') }}
                     </flux:link>
                 @endif
             </div>
 
-            <!-- Remember Me -->
-            <flux:checkbox name="remember" :label="__('Remember me')" :checked="old('remember')" />
+            {{-- Always remember login --}}
+            <input type="hidden" name="remember" value="1">
 
             <div class="flex items-center justify-end">
-                <flux:button variant="primary" type="submit" class="w-full" data-test="login-button">
+                <flux:button
+                    variant="primary"
+                    type="submit"
+                    class="w-full"
+                    data-test="login-button"
+                >
                     {{ __('Log in') }}
                 </flux:button>
             </div>
@@ -53,7 +62,9 @@
 
         <div class="space-x-1 text-sm text-center rtl:space-x-reverse text-zinc-600 dark:text-zinc-400">
             <span>{{ __('Don\'t have an account?') }}</span>
-            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+            <flux:link :href="route('register')" wire:navigate>
+                {{ __('Sign up') }}
+            </flux:link>
         </div>
     </div>
 </x-layouts::auth>
